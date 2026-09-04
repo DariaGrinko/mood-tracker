dc5b5229736ef24306b379bfb5c1f55354bf709f
DariaGrinko <silmarille333@gmail.com>
2026-09-04 17:18:16 +0300

    Refine mood chart and calendar layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,55 @@ import './App.css'
 import Calendar from 'react-calendar'
 import './Calendar.css'
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, usePlotArea, } from 'recharts';
 import { IconCalendarWeek } from '@tabler/icons-react'
 
 const moodData = [
-  { date: 'Aug 19', mood: 4 },
-  { date: 'Aug 20', mood: 5 },
-  { date: 'Aug 21', mood: 3 },
-  { date: 'Aug 22', mood: 10 },
-  { date: 'Aug 23', mood: 2 },
+  { date: 'Aug 19', mood: 8 },
+  { date: 'Aug 20', mood: 2 },
+  { date: 'Aug 21', mood: 4 },
+  { date: 'Aug 22', mood: 6 },
+  { date: 'Aug 23', mood: 7 },
   { date: 'Aug 24', mood: 5 },
-  { date: 'Aug 25', mood: 1 },
+  { date: 'Aug 25', mood: 10 },
 ]
 
+function MoodGradient() {
+  const plotArea = usePlotArea()
+
+  if (!plotArea) return null
+
+  const plotTop = plotArea.y
+  const plotBottom = plotArea.y + plotArea.height
+
+  return (
+    <defs>
+      <linearGradient
+        id="moodGradient"
+        x1="0"
+        y1={plotBottom}
+        x2="0"
+        y2={plotTop}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="10%" stopColor="#f52626" /> //10% doesn't equal 1
+        <stop offset="20%" stopColor="#f57c26" />
+
+        <stop offset="30%" stopColor="#e89d25" />
+        <stop offset="40%" stopColor="#f9db16" />
+
+        <stop offset="50%" stopColor="#d3f223" />
+        <stop offset="60%" stopColor="#84f01f" />
+
+        <stop offset="70%" stopColor="#84cc19" />
+        <stop offset="80%" stopColor="#5fcc16" />
+
+        <stop offset="90%" stopColor="#22c54b" />
+        <stop offset="100%" stopColor="#1ba24a" />
+      </linearGradient>
+    </defs>
+  )
+}
 
 function App() {
   return (
@@ -22,12 +58,11 @@ function App() {
       <aside className="sidebar">
 
         <div className="block" id='calendar'>
-          <div className='blockHeader'>
-            <IconCalendarWeek/>          
-            <p className='blockTitle'>Calendar</p>
-          </div>
 
-          <Calendar value={new Date()} />
+          <Calendar 
+          value={new Date()}
+           showFixedNumberOfWeeks
+            />
         </div>
 
         <div className="block" id='moodChart'>
@@ -40,31 +75,7 @@ function App() {
             data={moodData}
             margin={{ left: -38, right: 0 }}
           >
-            <defs>
-              <linearGradient
-                id="moodGradient"
-                x1="0"
-                y1="140"
-                x2="0"
-                y2="0"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0%" stopColor="#f52626" />
-                <stop offset="20%" stopColor="#f52626" />
-
-                <stop offset="30%" stopColor="#f56826" />
-                <stop offset="40%" stopColor="#f9b516" />
-
-                <stop offset="50%" stopColor="#eff223" />
-                <stop offset="60%" stopColor="#8bf01f" />
-
-                <stop offset="70%" stopColor="#84cc19" />
-                <stop offset="80%" stopColor="#5fcc16" />
-
-                <stop offset="90%" stopColor="#22c54b" />
-                <stop offset="100%" stopColor="#1ba24a" />
-              </linearGradient>
-            </defs>
+            <MoodGradient></MoodGradient>
 
             <XAxis
               dataKey="date"
@@ -74,7 +85,7 @@ function App() {
             />
             <YAxis
               domain={[0, 10.5]}
-              ticks={[0, 5, 10]}
+              ticks={[1, 5, 10]}
               tick={{ fontSize: 12 }}
             />
             <Line
@@ -87,7 +98,9 @@ function App() {
           </LineChart>
         </div>
 
-        <div className="block" id='sphereStats'>Sphere Stats</div>
+        <div className="block" id='sphereStats'>
+          <p className='blockTitle'>Spere Stats</p>
+        </div>
 
         <div className="block" id='topEmotions'>Top Emotions</div>
       </aside>
@@ -95,8 +108,10 @@ function App() {
       <div className="content">
 
         <main className="main">
+          <div className="block" id='selectedDate'>Selected Date</div>
           <div className="block" id='dailyMood'>Daily Mood</div>
-          <div className="block">Mood Entry</div>
+          <div className="block"
+          id='moodEntry'>Mood Entry</div>
         </main>
       </div>
     </div>
